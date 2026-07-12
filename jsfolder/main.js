e0f1affd29c5ab762de087ee782bc0ccbaab041e
hyunock <ryeohwin9@gmail.com>
2026-07-12 23:13:50 +0900

    feat: main.js setting

--- jsfolder/main.js
+++ jsfolder/main.js
@@ -1,1 +1,34 @@
 // 초기화 및 전체 실행 JS 파일
+
+// ===== Import =====
+
+// Storage
+import { loadTodos } from './storage.js';
+
+// Header
+import { initHeader } from './header.js';
+
+// main
+
+// dashboard
+
+
+// ===== Global Sate =====
+let state = {
+    todos: []
+};
+
+
+// ===== Initialize =====
+function init() {
+    // 저장된 데이터 가져오기
+    state.todos = loadTodos();
+    // ** 나중에 각 파일에서 export한 init 함수명과 일치하는지 확인
+    initHeader(state.todos);
+    // initStatistics(state.todos);
+    // initTodo(state.todos);
+}
+
+
+// ===== App Start =====
+document.addEventListener('DOMContentLoaded', init);
