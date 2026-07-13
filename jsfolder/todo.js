2d729af07aa9c34b7c5d015dc0fd820ab70afeb4
hyunock <ryeohwin9@gmail.com>
2026-07-13 11:18:20 +0900

    feat: board, controls, stats, todo JS 파일 추가

---
+++ jsfolder/todo.js
@@ -0,0 +1,12 @@
+// Todo 데이터 관리(CRUD)
+// - Todo 생성(create)
+// - Todo 조회(Read)
+// - Todo 수정(Update)
+// - Todo 삭제(Delete)
+// - Todo 상태 변경(TODO/ In Progress / Done)
+// createdAt, updatedAt, completedAt 관리
+
+// board.js: 화면 렌더링
+// status.js: 통계 계산
+// controls.js: 검색, 필터, 정렬
+// modal.js: 모달
